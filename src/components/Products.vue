ceb9d69efb0e791b200908707c5c93e5e6d39106
Damian Cisternas <decxx1@gmail.com>
2024-04-25 17:57:51 -0300

    container fix

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -78,14 +78,8 @@ const handleBrandChange = (value) => {
 </script>
 <template>
     <section class="mt-32">
-        <Pagination
-            client:load
-            :total="totalPages"
-            :current="currentPage"
-            :onPageChange="handlePageChange"
-            extraClass="mb-8"
-        />
-        <section class="bg-gray-50 dark:bg-gray-800 w-full">
+        
+        <section class="md:w-8/12 w-11/12 mx-auto overflow-hidden bg-white shadow-md dark:bg-gray-800 md:rounded-lg">
             <div class="md:w-8/12 w-11/12 mx-auto flex flex-column sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-center lg:justify-between pb-4">
                 <div>
                     <Filters 
@@ -101,15 +95,29 @@ const handleBrandChange = (value) => {
                         :onChange="handleBrandChange"
                     />
                 </div>
-                <div class="relative">
-                    <div class="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
-                        <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                <div>
+                    <label class="block mb-2 text-sm font-semibold text-gray-900 dark:text-white">Buscar</label>
+                    <div class="relative">
+                        <div class="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
+                            <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                        </div>
+                        <input type="text" id="table-search" class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg md:w-80 max-w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Buscar productos" 
+                        />
                     </div>
-                    <input type="text" id="table-search" class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg md:w-80 max-w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for items">
                 </div>
+                
+                <Pagination
+                    client:load
+                    :total="totalPages"
+                    :current="currentPage"
+                    :onPageChange="handlePageChange"
+                    extraClass="mx-auto border-t"
+                />
             </div>
+            
         </section>
-        <div class="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-y-2 gap-x-4 justify-items-center">
+        <div class="container mx-auto grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-y-2 gap-x-4 justify-items-center">
             <template v-for="product in paginateProducts" :key="product">
                 <Product
                     :image="product.img"
