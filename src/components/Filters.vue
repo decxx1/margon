4dadc52af5a90f6e47a1024a2f3ef51a973fe9e3
Damian Cisternas <decxx1@gmail.com>
2024-04-30 12:51:10 -0300

    design filters

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -52,32 +52,33 @@ onMounted(() => {
 <template>
     <div class="inline-flex items-center justify-center p-4">
         <div>
-            <label class="block mb-2 text-sm font-semibold text-gray-900 dark:text-white">Filtrar por {{ title }}</label>
-            <button :id="'dropFilters'+title"
-                class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+            <label class="block mb-2 text-sm font-semibold text-primary-950 dark:text-white">Filtrar por {{ title }}</label>
+            <button 
+                :id="'dropFilters'+title"
+                class="inline-flex items-center text-primary-950 bg-primary-50 border border-primary-200 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
                 type="button"
             >
-                <svg class="w-3 h-3 text-gray-500 dark:text-gray-400 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <svg class="w-3 h-3 text-primary-900 dark:text-gray-400 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                     <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M18.796 4H5.204a1 1 0 0 0-.753 1.659l5.302 6.058a1 1 0 0 1 .247.659v4.874a.5.5 0 0 0 .2.4l3 2.25a.5.5 0 0 0 .8-.4v-7.124a1 1 0 0 1 .247-.659l5.302-6.059c.566-.646.106-1.658-.753-1.658Z"/>
                 </svg>
 
                     {{ currentFilter === 'all' ? 'Todos' : currentFilter }}
-                <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                <svg class="w-2.5 h-2.5 ms-2.5 text-primary-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                 </svg>
             </button>
 
             <!-- Dropdown menu -->
             <div :id="'dropdownFilters' + title" class="z-10 hidden w-56 p-3 bg-white rounded-lg shadow dark:bg-gray-700">
-                <h6 class="mb-3 text-sm font-bold text-gray-900 dark:text-white">
+                <h6 class="mb-3 text-sm font-bold text-primary-950 dark:text-white">
                 {{ title }}
                 </h6>
-                <ul class="py-3 space-y-1 text-sm text-gray-700 dark:text-gray-200" :aria-labelledby="'dropFilters'+title">
+                <ul class="py-3 space-y-1 text-sm text-primary-950 dark:text-gray-200" :aria-labelledby="'dropFilters'+title">
 
                     <li>
-                        <div @click="handleFilterChange('all')" :class="[currentFilter === 'all' ? 'active' : '', 'cursor-pointer flex items-center py-2 rounded hover:bg-primary-100 dark:hover:bg-primary-600 [&.active]:bg-primary-100 dark:[&.active]:bg-primary-600']">
+                        <div @click="handleFilterChange('all')" :class="[currentFilter === 'all' ? 'active' : '', 'cursor-pointer flex items-center py-2 rounded border-primary-200 hover:bg-primary-100 dark:hover:bg-primary-600 [&.active]:bg-primary-100 dark:[&.active]:bg-primary-600']">
                             <label
-                                class="cursor-pointer w-full ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300"
+                                class="cursor-pointer w-full ms-2 text-sm font-medium text-primary-950 rounded dark:text-gray-300"
                             >Todas
                             </label>
                             <span class="text-center text-xs leading-4 px-1 text-primary-900 dark:text-primary-200 bg-primary-100 border-primary-300 rounded focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-primary-700 dark:focus:ring-offset-primary-700 focus:ring-2 dark:bg-primary-600 dark:border-primary-500"
@@ -86,9 +87,9 @@ onMounted(() => {
                     </li>
                     <li v-for="(item, index) in items" :key="index">
                         
-                        <div @click="handleFilterChange(item.name)" :class="[currentFilter === item.name ? 'active' : '', 'cursor-pointer flex items-center py-2 rounded hover:bg-gray-100 dark:hover:bg-primary-600 [&.active]:bg-primary-100 dark:[&.active]:bg-primary-600']">
+                        <div @click="handleFilterChange(item.name)" :class="[currentFilter === item.name ? 'active' : '', 'cursor-pointer flex items-center py-2 rounded border-primary-200 hover:bg-gray-100 dark:hover:bg-primary-600 [&.active]:bg-primary-100 dark:[&.active]:bg-primary-600']">
                             <label
-                                class="cursor-pointer w-full ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300"
+                                class="cursor-pointer w-full ms-2 text-sm font-medium text-primary-950 rounded dark:text-gray-300"
                             >{{item.name}}
                             </label>
                             <span class="text-center text-xs leading-4 px-1  text-primary-900 dark:text-primary-200 bg-primary-100 border-primary-300 rounded focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-primary-700 dark:focus:ring-offset-primary-700 focus:ring-2 dark:bg-primary-600 dark:border-primary-500"
--- src/components/Products.vue
+++ src/components/Products.vue
@@ -95,12 +95,12 @@ const handleBrandChange = (value) => {
                     />
                 </div>
                 <div class="md:pb-4 lg:pb-0">
-                    <label class="block mb-2 text-sm font-semibold text-gray-900 dark:text-white">Buscar</label>
+                    <label class="block mb-2 text-sm font-semibold text-primary-950 dark:text-white">Buscar</label>
                     <div class="relative">
                         <div class="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
-                            <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                            <svg class="w-5 h-5 text-primary-950 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
                         </div>
-                        <input type="text" id="table-search" class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg md:w-80 max-w-80 bg-gray-50 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                        <input type="text" id="table-search" class="block p-2 ps-10 text-sm text-primary-950 placeholder-primary-950 border border-primary-200 rounded-lg md:w-80 max-w-80 bg-primary-50 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                             placeholder="Buscar productos" 
                         />
                     </div>
