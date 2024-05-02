a154eeb035cd68ae5a947e8d75ed5c078a12bcae
Damian Cisternas <decxx1@gmail.com>
2024-05-01 21:05:40 -0300

    Search products logic

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -7,6 +7,7 @@ import Filters from '@/components/Filters.vue';
 import { onMounted } from 'vue';
 
 
+
 onMounted(() => {
     categories.value = calculateCounts('category');
     brands.value = calculateCounts('brand');
@@ -19,11 +20,13 @@ const categories = ref([]);
 const brands = ref([]);
 const currentCategory = ref('all');
 const currentBrand = ref('all');
+const searchProduct = ref('');
 //console.log(totalPages)
 const paginateProducts = computed ( () => {
     const startIndex = (currentPage.value - 1) * perPage;
     const endIndex = startIndex + perPage;
-
+    let input = searchProduct.value.toLowerCase();
+    
     // Filtrar productos por la categoría actual
     let filteredProducts = products;
     if (currentCategory.value !== 'all') {
@@ -32,8 +35,13 @@ const paginateProducts = computed ( () => {
     if (currentBrand.value !== 'all' ) {
         filteredProducts = filteredProducts.filter(product => product.brand === currentBrand.value);
     }
-
+    if (input.length > 0) {
+        filteredProducts = filteredProducts.filter(function(product) {
+            return product.title.toLowerCase().includes(input);
+        });
+    }
     totalPages.value = Math.ceil(filteredProducts.length / perPage );
+    if (currentPage.value > totalPages.value) handlePageChange(1) 
     return filteredProducts.slice(startIndex, endIndex);
 })
 
@@ -77,7 +85,7 @@ const handleBrandChange = (value) => {
 
 </script>
 <template>
-    <section class="mt-32">
+    <section class="mt-8">
         <section class="md:w-8/12 w-11/12 mx-auto overflow-hidden bg-white shadow border border-primary-200 dark:bg-gray-800 rounded-3xl px-10">
             <div class="w-full mx-auto px-4 flex flex-column sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-center lg:justify-between pb-4">
                 <div>
@@ -100,7 +108,7 @@ const handleBrandChange = (value) => {
                         <div class="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
                             <svg class="w-5 h-5 text-primary-950 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
                         </div>
-                        <input type="text" id="table-search" class="block p-2 ps-10 text-sm text-primary-950 placeholder-primary-950 border border-primary-200 rounded-lg md:w-80 max-w-80 bg-primary-50 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                        <input type="text" v-model="searchProduct" class="block p-2 ps-10 text-sm text-primary-950 placeholder-primary-950 border border-primary-200 rounded-lg md:w-80 max-w-80 bg-primary-50 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                             placeholder="Buscar productos" 
                         />
                     </div>
