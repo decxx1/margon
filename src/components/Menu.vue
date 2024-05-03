bf999de821a70dc545f32f2eca5af14e63992c44
Damian Cisternas <decxx1@gmail.com>
2024-05-03 19:47:57 -0300

    Distribuidores page

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -20,6 +20,11 @@ onMounted(() => {
                 >Catálogo</a>
         </li>
         <li>
+            <a href="/distribuidores"
+            :class="[path === '/distribuidores' ? 'bg-primary-700 text-white lg:text-primary-700 dark:text-white lg:bg-transparent rounded' : 'text-primary-950 hover:bg-gray-50', 'block py-2 pr-4 pl-3  border-b border-gray-100  lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700']"
+                >Distribuidores</a>
+        </li>
+        <li>
             <a href="/contacto"
             :class="[path === '/contacto' ? 'bg-primary-700 text-white lg:text-primary-700 dark:text-white lg:bg-transparent rounded' : 'text-primary-950 hover:bg-gray-50', 'block py-2 pr-4 pl-3  border-b border-gray-100  lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700']"
                 >Contacto</a>
